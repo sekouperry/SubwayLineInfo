bf274fed46b639c8155d56e564d73e760fe88dc5
RBlafford <rblafford@gmail.com>
2014-11-04 12:01:09 -0500

    Initial Commit

---
+++ YahooSubwayTask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YahooSubwayTask
+//
+//  Created by Robert Blafford on 11/4/14.
+//  Copyright (c) 2014 Robert Blafford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YahooSubwayTask/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YahooSubwayTask
+//
+//  Created by Robert Blafford on 11/4/14.
+//  Copyright (c) 2014 Robert Blafford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
